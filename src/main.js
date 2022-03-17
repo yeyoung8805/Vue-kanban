d3edf1942dfa5d7f80be7967639ed2525ed5af72
yeyoung8805 <yyk8805@naver.com>
2022-03-17 15:43:57 +0900

    add Vuex store instance, and Promise polyfill

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import 'es6-promise/auto' //Promise 폴리필
 import App from './App'
 import router from './router'
+import store from './store' //Vuex 스토어 인스턴스 임포트
 
-Vue.config.productionTip = false
+Vue.config.productionTip = process.env.MODE_ENV === 'production'
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
