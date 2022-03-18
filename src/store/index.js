091c0b203468f4bce4fc672411a11055e726a4f0
yeyoung8805 <yyk8805@naver.com>
2022-03-18 09:52:42 +0900

    edit store.js to initialize

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,19 @@ import mutations from './mutations'
 
 Vue.use(Vuex)
 
+//상태 'Auth'와 상태 'Board' 의 Vuex state를 한곳에서 관리할 수 있도록 정의
+const state = {
+    auth: { //상태 Auth
+        token: null, //token을 null로 초기화
+        userId: null //userId를 null 로 초기화
+    },
+    board: { //상태 board
+        lists: [] //상태 TaskList는 빈 배열로 초기화
+    }
+}
+
 export default new Vuex.Store({
+    state, //앞서 정의한  state를  'state' 옵션으로  지정
     getters,
     actions,
     mutations,
