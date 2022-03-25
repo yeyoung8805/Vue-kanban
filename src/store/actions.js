4436469bab19544bf62365caf8d20c075c5137b4
yeyoung8805 <yyk8805@naver.com>
2022-03-25 17:35:54 +0900

    edit inside actions.js

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,34 +1,66 @@
-/* eslint-disable no-unused-vars */
 import * as types from './mutation-types'
 import {Auth, List, Task} from '../api'
-/* eslint-enable no-unused-vars */
 
 export default {
     login: ({commit}, authInfo) => {
         return Auth.login(authInfo)
             .then(({token, userId}) => {
+                localStorage.setItem('token', token)
                 commit(types.AUTH_LOGIN, {token, userId})
             })
             .catch(err => {throw err})
     },
-    fetchLists: ({commit}) => {
-        //TODO :
-        throw new Error('fetchLists action should be implmented')
+    fetchLists: ({commit, state}) => {
+        return List.fetch(state.auth.token)
+            .then(({lists}) => {
+                commit(types.FETCH_ALL_TASKLIST, lists)
+            })
+            .catch(err => {throw err})
     },
-    addTask: ({commit}) => {
-        //TODO :
-        throw new Error('addTask action should be implmented')
+    addTask: ({commit, state}, {listId, name}) => {
+        return Task.add(state.auth.token, {listId, name})
+            .then(() => {
+                commit(types.ADD_TASK, task)
+            })
+            .catch(err => {throw err})
     },
-    updateTask: ({commit}) => {
-        //TODO :
-        throw new Error('updateTask action should be implmented')
+    updateTask: ({commit, state}, task) => {
+        return Task.update(state.auth.token, task)
+            .then(() => {
+                commit(types.UPDATE_TASK, task)
+            })
+            .catch(err => {throw err })
     },
-    removeTask: ({commit}) => {
-        //TODO :
-        throw new Error('removeTask action should be implmented')
+    removeTask: ({commit, state}, {id, listId}) => {
+        return Task.remove(state.auth.token, {id,listId})
+            .then(() => {
+                commit(types.REMOVE_TASK, {id, listId})
+            })
+            .catch(err => {throw err })
     },
-    logout: ({commit}) => {
-        //TODO :
-        throw new Error('logout action should be implmented')
+    moveTaskFrom : ({commit, state}, {id,listId}) => {
+        commit(types.MOVE_TASK_FROM, {target: id, from: listId})
+        return Promise.resolve()
+    },
+    movetoTask : ({commit, state}, {id, listId}) => {
+        commit(types.MOVE_TO_TASK, {target: id, to: listId})
+        return Promise.resolve()
+    },
+    performTaskMoving: ({commit, state}) => {
+        const {target, from, to} = state.draggging
+        return Task.move(state.auth.token, {id:target, from, to})
+            .then(() => {
+                commit(types.MOVE_TASK_DONE, {target, from, to})
+            })
+            .catch(err => {throw err})
+    },
+
+    logout: ({commit, state}) => {
+        return Auth.logout(state.auth.token)
+            .then(() => {
+                localStorage.removeItem('token')
+                commit(types.AUTH_LOGOUT, {token: null, userId: null})
+            })
+            .catch(err => {throw err})
     },
 }
